b3c9bc58c669150ee530e23db65c998f9a887862
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-10-25 19:53:41 -0400

    lesson 4

--- starter-files/sanity/sample-data/delete-data-example.js
+++ starter-files/sanity/sample-data/delete-data-example.js
@@ -14,7 +14,7 @@
 import client from 'part:@sanity/base/client';
 
 client
-  // .delete({ query: '*[!defined(name) && _type == "topping"] ' })
-  .delete({ query: '*[_type == "pizza"] ' })
-  .then(console.log)
-  .catch(console.error);
+	// .delete({ query: '*[!defined(name) && _type == "topping"] ' })
+	.delete({ query: '*[_type == "pizza"] ' })
+	.then(console.log)
+	.catch(console.error);
