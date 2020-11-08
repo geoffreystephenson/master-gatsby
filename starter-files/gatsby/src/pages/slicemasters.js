83e18172ebc8e92cdbfae5f8991df9e5370bd25d
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-11-07 19:18:51 -0500

    WIP

--- starter-files/gatsby/src/pages/slicemasters.js
+++ starter-files/gatsby/src/pages/slicemasters.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function SlicemastersPage() {
 	return (
 		<div>
-			<p>Hey! I'm the Slicemasters page.</p>
+			<p>Hey! I'm the Slicemasters page</p>
 		</div>
 	);
 }
