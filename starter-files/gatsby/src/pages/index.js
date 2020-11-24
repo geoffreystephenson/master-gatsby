e3a6741d4c927f24bdafd0b68143786817a41902
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-11-23 19:05:47 -0500

    lessons 5-6

--- starter-files/gatsby/src/pages/index.js
+++ starter-files/gatsby/src/pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Layout from '../components/Layout';
 
 export default function HomePage() {
 	return (
-		<div>
+		<Layout>
 			<p>Hey! I'm the home page</p>
-		</div>
+		</Layout>
 	);
 }
--- starter-files/gatsby/src/pages/slicemasters.js
+++ starter-files/gatsby/src/pages/slicemasters.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function SlicemastersPage() {
 	return (
