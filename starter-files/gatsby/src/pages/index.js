06237d035ef0734ab53c8bbd2c8be603834decda
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-11-24 20:06:02 -0500

    tweak

--- starter-files/gatsby/src/pages/index.js
+++ starter-files/gatsby/src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function HomePage() {
-	return <>Hey! I'm the home page.</>;
+	return <>Hey! I'm the home page</>;
 }
--- starter-files/gatsby/src/pages/order.js
+++ starter-files/gatsby/src/pages/order.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function OrderPage() {
 	return (
 		<>
-			<p>Hey! I'm the Order page.</p>
+			<p>Hey! I'm the Order page</p>
 		</>
 	);
 }
