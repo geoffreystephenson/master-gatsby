0c7e5ed591f2f3968d92af2e41ebbd949047de15
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-12-04 20:49:26 -0500

    lesson 16

--- starter-files/gatsby/gatsby-config.js
+++ starter-files/gatsby/gatsby-config.js
@@ -1,1 +1,10 @@
 // This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+
+export default {
+	siteMetadata: {
+		name: `Slicks Slices`,
+		siteUrl: 'https://gatsby.pizza',
+		description: 'The best pizza place in Hamilton!',
+		twitter: '@slicksSlices',
+	},
+};
