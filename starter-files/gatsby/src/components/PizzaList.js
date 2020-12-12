af545270f9bd7c0ecb0218411966af85b3596b96
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-12-11 20:05:27 -0500

    lesson 18

--- starter-files/gatsby/src/components/PizzaList.js
+++ starter-files/gatsby/src/components/PizzaList.js
@@ -2,7 +2,18 @@ import { Link } from 'gatsby';
 import React from 'react';
 
 function SinglePizza({ pizza }) {
-	return <div></div>;
+	return (
+		<div>
+			<Link to={`/pizza/${pizza.slug.current}`}>
+				<h2>
+					<span className="mark">{pizza.name}</span>
+				</h2>
+				<p>
+					{pizza.toppings.map((topping) => topping.name).join(', ')}
+				</p>
+			</Link>
+		</div>
+	);
 }
 
 export default function PizzaList({ pizzas }) {
