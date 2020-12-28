4a415751b8443f1e2f441b3b74ff8a2ca7e748ce
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-12-28 17:32:42 -0500

    lesson 28

--- starter-files/gatsby/src/pages/pizzas.js
+++ starter-files/gatsby/src/pages/pizzas.js
@@ -4,7 +4,6 @@ import PizzaList from '../components/PizzaList';
 import ToppingsFilter from '../components/ToppingsFilter';
 
 export default function PizzasPage({ data, pageContext }) {
-	// console.log(data);
 	const pizzas = data.pizzas.nodes;
 	return (
 		<>
