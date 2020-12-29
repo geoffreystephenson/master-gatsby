9376bdedcb90efb4aec92c2856b48a5073b4f3f5
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-12-29 17:52:39 -0500

    lesson 31

--- starter-files/gatsby/src/pages/slicemasters.js
+++ starter-files/gatsby/src/pages/slicemasters.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
+import Pagination from '../components/Pagination';
 
 const SlicemasterGrid = styled.div`
 	display: grid;
@@ -36,10 +37,17 @@ const SliceMasterStyles = styled.div`
 	}
 `;
 
-export default function SlicemastersPage({ data }) {
+export default function SlicemastersPage({ data, pageContext }) {
 	const slicemasters = data.slicemasters.nodes;
 	return (
 		<>
+			<Pagination
+				pageSize={parseInt(process.env.GATSBY_PAGE_SIZE)}
+				totalCount={data.slicemasters.totalCount}
+				currentPage={pageContext.currentPage || 1}
+				skip={pageContext.skip}
+				base="/slicemasters"
+			/>
 			<SlicemasterGrid>
 				{slicemasters.map((person) => (
 					<SliceMasterStyles key={person.id}>
