ba61fa63585b45054dd8e8e82539dc2a5cdc6b04
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2021-01-07 20:43:12 -0500

    lesson 41

--- starter-files/gatsby/functions/placeOrder/placeOrder.js
+++ starter-files/gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,28 @@
 const nodemailer = require('nodemailer');
 
+function generateOrderEmail({ order, total }) {
+	return `<div>
+        <h2>Your recent order for ${total}</h2>
+        <p>Please start walking over, we will have your order ready in the next 20 minutes!</p>
+        <ul>
+            ${order
+				.map(
+					(item) => `<li>
+                <img src="${item.thumbnail}" alt="${item.name}">
+                ${item.size} ${item.name} - ${item.price}
+            </li>`
+				)
+				.join('')}
+        </ul>
+        <style>
+            ul {
+                list-style: none;
+            }
+        </style>
+        <p>Your total is <strong>$${total}</strong> due at pickup</p>
+    </div>`;
+}
+
 // create a transport for nodemailer
 const transporter = nodemailer.createTransport({
 	host: process.env.MAIL_HOST,
@@ -11,16 +34,34 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.handler = async (event, context) => {
+	const body = JSON.parse(event.body);
+	// Validate the data coming in is correct
+	const requiredFields = ['email', 'name', 'order'];
+
+	for (const field of requiredFields) {
+		if (!body[field]) {
+			return {
+				statusCode: 400,
+				body: JSON.stringify(
+					`Ooops you are missing the ${field} field`
+				),
+			};
+		}
+	}
+	// send the email
+
+	// send the success or error message
+
 	// Test send email
 	const info = await transporter.sendMail({
-		from: "Slick's Slice <slick@example.com>",
-		to: 'orders@example.com',
+		from: "Slick's Slices <slick@example.com>",
+		to: `${body.name} <${body.email}>`,
 		subject: 'New order!',
-		html: `<p>Your new order!</p>`,
+		html: generateOrderEmail({ order: body.order, total: body.total }),
 	});
-	console.log(info);
+	// console.log(info);
 	return {
 		statusCode: 200,
-		body: JSON.stringify(info),
+		body: JSON.stringify({ message: 'Success' }),
 	};
 };
--- starter-files/gatsby/src/utils/usePizza.js
+++ starter-files/gatsby/src/utils/usePizza.js
@@ -33,7 +33,8 @@ export default function usePizza({ pizzas, values }) {
 		e.preventDefault();
 		setLoading(true);
 		setError(null);
-		setMessage(null);
+		// setMessage(null);
+		setMessage('Go eat!');
 
 		// gather all the data
 		const body = {
