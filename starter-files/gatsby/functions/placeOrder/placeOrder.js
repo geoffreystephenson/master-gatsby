3de82b06861260cc8f0c5f2d2fcc08e7bdbddb16
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2021-01-09 19:30:22 -0500

    lesson 43

--- starter-files/gatsby/functions/placeOrder/placeOrder.js
+++ starter-files/gatsby/functions/placeOrder/placeOrder.js
@@ -40,9 +40,15 @@ function wait(ms = 0) {
 }
 
 exports.handler = async (event, context) => {
-	await wait(5000);
+	// await wait(5000);
 	const body = JSON.parse(event.body);
-	console.log(body);
+	// Check if they have filled out the honeypot
+	if (body.mapleSyrup) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({ message: 'Boop beep bop zzzzstt good bye' }),
+		};
+	}
 	// Validate the data coming in is correct
 	const requiredFields = ['email', 'name', 'order'];
 
