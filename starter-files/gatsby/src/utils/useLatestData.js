76a0b1b8c914e0160589ec1feee2ef1f94dfef0d
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2021-01-16 18:25:39 -0500

    lesson 45

---
+++ starter-files/gatsby/src/utils/useLatestData.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+
+export default function useLatestData() {
+	// hot slices
+	const [hotSlices, setHotSlices] = useState();
+	// slicemasters
+	const [slicemasters, setSlicemasters] = useState();
+	// Use a side effect to fetcht he data from the graphql endpoint
+	useEffect(function () {
+		// when the component loads, fetch the data
+		fetch(process.env.GATSBY_GRAPHQL_ENDPOINT, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				query: `
+					query {
+  StoreSettings(id: "downtown") {
+    name
+    slicemaster {
+      name
+    }
+    hotSlices {
+      name
+    }
+  }
+
+}
+				`,
+			}),
+		}).then((res) =>
+			res.json().then((res) => {
+				// TO DO
+				// set data to state
+				setHotSlices(res.data.StoreSettings.hotSlices);
+				setSlicemasters(res.data.StoreSettings.slicemaster);
+			})
+		);
+	}, []);
+	return {
+		hotSlices,
+		slicemasters,
+	};
+}
